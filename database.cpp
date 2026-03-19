12387f893700a46f187f2d9970818a746a9382f7
iuniperum <julpietrzak147@gmail.com>
2026-03-19 10:16:40 +0100

    database

---
+++ database.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class cell {
+    int id;
+    cell* prev;
+    cell* next;
+};
+
+class list {
+    cell* head;
+    cell* tail;
+};
+
+void add (list* dest, cell* new_cell){
+    
+}
+
+class database {
+    cell* head;
+
+};
